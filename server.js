cfc5b289b1869e03df46da09b4c89534f25a4898
Harith009 <77931370+Harith009@users.noreply.github.com>
2021-09-30 22:52:09 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const app = express();
 const axios = require('axios');
 const port = 3000;
 
+app.get('/', function(req, res) {
+        res.send("Please visit http://143.244.167.45:3000/say?keyword=hello for the response from AWS Function");
+});
+
 app.get('/say', function(req,res) {
         resp = "Hello World" + req.query.keyword ;
         axios.get('https://au6nqdftl9.execute-api.us-east-1.amazonaws.com/default/my-function?keyword='+req.query.keyword)
